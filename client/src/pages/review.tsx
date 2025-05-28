77cd79cfb475d425021e09b59b82c5ff83cee846
s.nakamura <s.nakamura@lobbdesign.jp>
2025-05-28 12:45:06 +0900

    2025-05-28-1245

--- client/src/pages/review.tsx
+++ client/src/pages/review.tsx
@@ -1,4 +1,4 @@
-「Ctrl + C」import { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "wouter";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
